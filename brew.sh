d163177df8a2bd8189b51bfa4f55c3a18ed3b92c
Jennifer Cwagenberg <CandiedCode@gmail.com>
2020-11-15 10:44:40 -0600

    Add regularly used brew packages to brew.sh script

---
+++ brew.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+brew update && brew upgrade
+
+brew install awscli
+brew install coreutils
+brew install ed
+brew install findutils
+brew install gawk
+brew install gnu-sed
+brew install gnu-tar
+brew install gpg
+brew install graphviz
+brew install grep
+brew install hugo
+brew install kind
+brew install kubeval
+brew install kustomize
+brew install make
+brew install opa
+brew install protobuf
+brew install swagger-codegen
+brew install skaffold
+brew install tfswitch
+brew install terragrunt
+brew install tmux
+
+brew cleanup
