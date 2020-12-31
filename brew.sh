203cd604954c5e96820e32100e974b0e0fdc30eb
Jennifer Cwagenberg <CandiedCode@gmail.com>
2020-12-31 17:04:25 -0600

    Add additional go and brew packages

--- brew.sh
+++ brew.sh
@@ -4,6 +4,7 @@ brew update && brew upgrade
 
 brew install awscli
 brew install coreutils
+brew install earthly
 brew install ed
 brew install findutils
 brew install gawk
@@ -20,12 +21,14 @@ brew install kind
 brew install kubeval
 brew install kustomize
 brew install make
+brew install markdownlint-cli 
 brew install opa
 brew install pinentry-mac
 brew install protobuf
 brew install swagger-codegen
 brew install skaffold
 brew install shellcheck
+brew install tfsec
 brew install tfswitch
 brew install terragrunt
 brew install tmux
